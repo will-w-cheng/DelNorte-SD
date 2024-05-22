3eeba096ce7101de33f44b60c0c476afea667737
tarunja1ks <tarunjaiks@gmail.com>
2024-05-21 22:05:30 -0700

    scrolling initial homepage

--- static/script.js
+++ static/script.js
@@ -1,16 +1,18 @@
 document.addEventListener('DOMContentLoaded', () => {
     const events = document.querySelectorAll('.event');
     const observer = new IntersectionObserver(entries => {
-      entries.forEach(entry => {
-        if (entry.isIntersecting) {
-          entry.target.querySelector('.content').style.opacity = '1';
-          observer.unobserve(entry.target);  // Optionally stop observing the element once it has animated
-        }
-      });
+        entries.forEach(entry => {
+            if (entry.isIntersecting) {
+                entry.target.querySelector('.content').style.opacity = '1';
+                entry.target.querySelector('.content').style.color = '#0000FF';
+            } else {
+                entry.target.querySelector('.content').style.opacity = '0.5'; // Change back to original opacity
+                entry.target.querySelector('.content').style.color = '#FFFFFF'; // Change back to original color
+            }
+        });
     });
   
     events.forEach(event => {
-      observer.observe(event);
+        observer.observe(event);
     });
-  });
-  
+});
