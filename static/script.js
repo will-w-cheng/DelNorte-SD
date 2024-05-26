59944ebc8d873c29d29a0feec2c6a53099429a21
tarunja1ks <tarunjaiks@gmail.com>
2024-05-26 12:48:17 -0700

    so far commit

--- static/script.js
+++ static/script.js
@@ -11,8 +11,26 @@ document.addEventListener('DOMContentLoaded', () => {
             }
         });
     });
-  
     events.forEach(event => {
         observer.observe(event);
     });
+
+
+    const timeline = document.querySelector('.timeline');
+    const n = 5; // Number of sections
+
+    for (let i = 0; i < n; i++) {
+        const section = document.createElement('div');
+        section.classList.add('timeline-section');
+        section.style.width = `${100 / n}%`; // Dynamically set the width of each section
+        timeline.appendChild(section);
+    }
+
+
+    window.addEventListener('scroll', function () {
+        const scrollPercentage = (document.documentElement.scrollTop + document.body.scrollTop) / (document.documentElement.scrollHeight - document.documentElement.clientHeight) * 100;
+    });
+
 });
+
+
